358390883f05e167090ba93948a80e36cb20ae9d
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-04-13 19:50:09 +0900

    MODIFY: host view 중복 방지 추가 및 host column 추가

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from users.views import KaKaoSigninView
+from users.views import KaKaoSigninView, HostConvertView
 
 urlpatterns = [
     path('/signin/kakao', KaKaoSigninView.as_view()),
+    path('/host', HostConvertView.as_view()),
 ]
