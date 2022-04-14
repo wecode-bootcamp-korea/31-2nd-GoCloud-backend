07a4cfcf172ce84326f2ffb405662867938d6c08
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-04-14 16:13:27 +0900

    MODIFY: model 수정

--- spaces/models.py
+++ spaces/models.py
@@ -11,28 +11,16 @@ class Category(models.Model):
 class Space(TimeStamp): 
     host         = models.ForeignKey('users.Host', on_delete=models.CASCADE, related_name='spaces')
     title        = models.CharField(max_length=100)
+    sub_title    = models.CharField(max_length=100)
+    room_name    = models.CharField(max_length=45)
     detail       = models.TextField()
     max_capacity = models.IntegerField()
     address      = models.CharField(max_length=100)
+    price        = models.DecimalField(max_digits=30, decimal_places=2)
     category     = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='spaces')
 
     class Meta: 
         db_table = 'spaces'
-
-class Size(models.Model): 
-    square_feet = models.IntegerField(null=True)
-    
-    class Meta: 
-        db_table = 'sizes'
-
-class SpaceSize(models.Model): 
-    name  = models.CharField(max_length=100)
-    size  = models.ForeignKey(Size, on_delete=models.CASCADE, related_name='spacesizes')
-    space = models.ForeignKey(Space, on_delete=models.CASCADE, related_name='spacesizes')
-    price = models.DecimalField(max_digits=30, decimal_places=2)
-
-    class Meta: 
-        db_table = 'space_sizes'
 
 class Image(models.Model): 
     space = models.ForeignKey(Space, on_delete=models.CASCADE, related_name='images')
@@ -42,9 +30,10 @@ class Image(models.Model):
         db_table = 'images'
 
 class Review(TimeStamp): 
-    user    = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='reviews')
-    space   = models.ForeignKey(Space, on_delete=models.CASCADE, related_name='reviews')
-    content = models.TextField()
+    user      = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='reviews')
+    space     = models.ForeignKey(Space, on_delete=models.CASCADE, related_name='reviews')
+    content   = models.TextField()
+    image_url = models.CharField(max_length=200)
 
     class Meta: 
         db_table = 'reviews'
