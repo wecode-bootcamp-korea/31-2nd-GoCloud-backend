529fae07978426b49f0c9c2fd082e362a9ea794c
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-04-19 22:07:40 +0900

    ADD: space detail view

--- spaces/urls.py
+++ spaces/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from spaces.views import SpaceListView, ReviewView
+from spaces.views import SpaceListView, ReviewView, SpaceDetailView
 
 urlpatterns = [
     path('', SpaceListView.as_view()),
     path('/reviews', ReviewView.as_view()),
+    path('/detail/<int:space_id>', SpaceDetailView.as_view()),
 ]
