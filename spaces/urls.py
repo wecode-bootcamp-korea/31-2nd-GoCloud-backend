cbb9fe9c7e022444b3d2d601cadb7b4f8b8292e3
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-04-20 21:28:11 +0900

    modify2

--- spaces/urls.py
+++ spaces/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from spaces.views import SpaceListView, ReviewView, SpaceDetailView, BookingView
+from spaces.views import SpaceListView, ReviewView, SpaceDetailView, BookingView, BookingListView
 
 urlpatterns = [
     path('', SpaceListView.as_view()),
     path('/reviews', ReviewView.as_view()),
     path('/detail/<int:space_id>', SpaceDetailView.as_view()),
     path('/<int:space_id>/booking', BookingView.as_view()),
+    path('/bookinglist', BookingListView.as_view()),
 ]
