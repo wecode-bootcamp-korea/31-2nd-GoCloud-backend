d8c087a93b89bfc7b06bdb02bc37e2e39a9d4348
고현영 <hyunyoungko@gohyeon-yeong-ui-MacBookAir.local>
2022-04-20 20:36:56 +0900

    MODIFY: conflict 해결

--- spaces/urls.py
+++ spaces/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from spaces.views import SpaceListView, ReviewView, SpaceDetailView
+from spaces.views import SpaceListView, ReviewView, SpaceDetailView, BookingView
 
 urlpatterns = [
     path('', SpaceListView.as_view()),
     path('/reviews', ReviewView.as_view()),
     path('/detail/<int:space_id>', SpaceDetailView.as_view()),
+    path('/<int:space_id>/booking', BookingView.as_view()),
 ]
